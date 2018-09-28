adc84e41d6282ffed548fc891963eb129081bfc9
Ernanni <ernannicf@gmail.com>
2018-09-27 22:47:23 -0300

    rebase

--- app/src/main/java/edu/retrofit/API/helpers/Const.java
+++
@@ -1,4 +0,0 @@
-package edu.retrofit.API.helpers;
-
-public class Const {
-}
--- app/src/main/java/edu/retrofit/API/helpers/WebServiceInterface.java
+++
@@ -1,7 +0,0 @@
-package edu.retrofit.API.helpers;
-
-//integrar as camadas (entrega a resposta do controller p/ view)
-public interface WebServiceInterface {
-	void success(Object obj);
-	void erro(Throwable throwable);
-}
